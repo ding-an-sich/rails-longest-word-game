02a256af5f560ce0488e87e7bdc085e3885afc45
Vinicius Moraes <vmrc@ding-an-sich.com>
2020-08-24 16:34:39 -0300

    Tally a total score

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,6 @@
-require 'open-uri'
+# frozen_string_literal: true
 
+# Word game controller
 class GamesController < ApplicationController
   def new
     alphabet = ('A'..'Z').to_a
@@ -14,30 +15,29 @@ class GamesController < ApplicationController
     word = params[:word]
     english = english?(word)
     in_grid = grid?(word, letters)
-    session[:total_score] = session[:total_score].nil? ? 0 : session[:total_score]
+    session[:total_score] = init_total_score
     @score = english && in_grid ? word.length : 0
     session[:total_score] += @score
-    @end_string = build_string_get_rid_of_icecream(english, in_grid, word, letters)
+    @end_string = build_stringy_get_rid_of_icecreamy(english, in_grid, word, letters)
   end
 
   private
 
   def english?(word)
     url = "https://wagon-dictionary.herokuapp.com/#{word}"
-    data = open(url).read
-    wagon_json = JSON.parse(data)
-    wagon_json["found"]
+    wagon_json = JSON.parse(URI.parse(url).read)
+    wagon_json['found']
   end
 
   def grid?(word, letters)
-    word_array = word.upcase.split("")
-    word_array.all? do |char|
+    word_array = word.upcase.split('')
+    word_array.all? do |char| # Check if every char in the word matches the two conditions
       letters.include? char
       word_array.count(char) <= letters.count(char)
     end
   end
 
-  def build_string_get_rid_of_icecream(english, in_grid, word, letters)
+  def build_stringy_get_rid_of_icecreamy(english, in_grid, word, letters)
     if english && in_grid
       "Congratulations! #{word.upcase} is a valid English word!"
     elsif english
@@ -45,5 +45,9 @@ class GamesController < ApplicationController
     else
       "Sorry, but #{word.upcase} does not seem to be a valid English word..."
     end
+  end
+
+  def init_total_score
+    session[:total_score].nil? ? 0 : session[:total_score]
   end
 end
